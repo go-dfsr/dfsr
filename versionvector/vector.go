dd8ae0f423c6b9fb7e0f2c7f296c31cf8ff2d959
Joshua Sjoding <joshua.sjoding@scjalliance.com>
2016-08-11 18:02:58 -0700

    Added Vector.Duplicate() function

--- versionvector/vector.go
+++ versionvector/vector.go
@@ -1,6 +1,9 @@
 package versionvector
 
-import "github.com/go-ole/go-ole"
+import (
+	"github.com/go-ole/go-ole"
+	"github.com/scjalliance/comutil"
+)
 
 // Vector represents version vector data from a replication group member.
 type Vector struct {
@@ -17,6 +20,17 @@ func New(sa *ole.SafeArrayConversion) (vector *Vector, err error) {
 // Data returns the version vector data as a safe array.
 func (vector *Vector) Data() (sa *ole.SafeArrayConversion) {
 	return vector.sa
+}
+
+// Duplicate will return a duplicate of the vector that does not share any
+// memory with the original.
+func (vector *Vector) Duplicate() (duplicate *Vector, err error) {
+	sa, err := comutil.SafeArrayCopy(vector.Data().Array)
+	if err != nil {
+		return
+	}
+
+	return New(&ole.SafeArrayConversion{Array: sa})
 }
 
 // Close will release any resources consumed by the version vector. It should be
