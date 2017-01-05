fc3a07692725914bd93b060701a2b6cc900eff5f
Joshua Sjoding <joshua.sjoding@scjalliance.com>
2017-01-04 20:31:19 -0800

    poller refactor and fixed context cancellation handling

--- poller/poller.go
+++ poller/poller.go
@@ -14,17 +14,22 @@ type Source interface {
 
 // Poller executes a polling function on an interval.
 type Poller struct {
-	mutex    sync.Mutex
 	interval time.Duration
 	source   Source
-	updating bool
-	pulse    chan struct{} // Signals update. nil indicates closed.
-	stop     chan struct{} // Signals stop. nil indicates stopped.
-	idle     *sync.Cond
-	closed   bool
+
+	mutex  sync.Mutex
+	cancel context.CancelFunc // Cancellation function. Nil when not running.
+	pulse  chan struct{}      // Signals update. nil indicates closed.
+	stop   chan struct{}      // Signals stop. nil indicates stopped.
+	idle   *sync.Cond
+	closed bool
 }
 
-// New returns a new poller for the given source.
+// New returns a new poller for the given source. A ticker with the provided
+// polling interval will be started immediately, but an invocation of the
+// polling function will not run until its interval has elapsed. If immediate
+// invocation is desired the Poll function should be called immediately after
+// the poller has been created.
 func New(source Source, interval time.Duration) *Poller {
 	p := &Poller{
 		source:   source,
@@ -53,9 +58,13 @@ func (p *Poller) Close() {
 	close(p.stop)
 	close(p.pulse)
 
+	if p.running() {
+		p.cancel()
+	}
+
 	// If there's an update goroutine still running, wait until it's done before
 	// closing the source.
-	for p.updating {
+	for p.running() {
 		p.idle.Wait()
 	}
 
@@ -77,29 +86,29 @@ func (p *Poller) run() {
 	ticker := time.NewTicker(p.interval)
 	defer ticker.Stop()
 
-	var (
-		ctx    context.Context
-		cancel context.CancelFunc
-	)
-
 	for {
-		ctx, cancel = context.WithCancel(context.Background())
 		select {
 		case <-p.stop:
-			cancel()
 			return
 		case <-p.pulse:
 		case <-ticker.C:
 		}
 
-		go p.update(ctx, cancel)
+		go p.invoke()
 	}
 }
 
-func (p *Poller) update(ctx context.Context, cancel context.CancelFunc) {
+// running returns true if an update is running. It must be called while
+// a lock on the poller's mutex is held.
+func (p *Poller) running() bool {
+	return p.cancel != nil
+}
+
+func (p *Poller) invoke() {
+	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	if !p.startUpdate() {
+	if !p.startInvocation(cancel) {
 		// There is an update goroutine already running, so we're skipping this
 		// tick so that we don't spawn doubles
 		return
@@ -107,22 +116,22 @@ func (p *Poller) update(ctx context.Context, cancel context.CancelFunc) {
 
 	p.source.Poll(ctx)
 
-	p.finishUpdate()
+	p.finishInvocation()
 }
 
-func (p *Poller) startUpdate() (acquired bool) {
+func (p *Poller) startInvocation(cancel context.CancelFunc) (acquired bool) {
 	p.mutex.Lock()
-	if !p.closed && !p.updating {
-		p.updating = true
+	if !p.closed && !p.running() {
+		p.cancel = cancel
 		acquired = true
 	}
 	p.mutex.Unlock()
 	return
 }
 
-func (p *Poller) finishUpdate() {
+func (p *Poller) finishInvocation() {
 	p.mutex.Lock()
-	p.updating = false
+	p.cancel = nil
 	p.mutex.Unlock()
 	p.idle.Broadcast()
 }
